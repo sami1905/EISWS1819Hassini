e3cf470143a2ceecf18138dfe99ff71b3431f23d
shassini <s.hassini@gmx.de>
2019-01-12 03:33:41 +0100

    database extended. dexcomValues and userValues merged

--- Abgabe_MS3/Implementation/Server/app.js
+++ Abgabe_MS3/Implementation/Server/app.js
@@ -5,6 +5,8 @@ var qs = require("querystring");
 var http = require("https");
 var dexcomHTTP = require("https");
 
+
+
 var app = express();
 app.use(bodyParser.json());
 
@@ -12,7 +14,8 @@ const settings = {
     port: 3000,
     dexcom_data: './dexcom_data.json',
     user_events: './user_events.json',
-    user_Authorization: './user_Authorization.json'
+    user_values: './user_values.json',
+    user_authorization: './user_Authorization.json'
 };
 
 app.use(function(err, req, res, next){
@@ -25,6 +28,64 @@ app.use(function(req, res, next){
   next();
 });
 
+//POST /userValues
+
+app.post('/userValues', bodyParser.json(),function(req, res){
+    fs.readFile(settings.user_values, function(err, data){
+        var userValues = JSON.parse(data);
+        var numberOfValues = userValues.length;
+        var lastDate;
+        var userValuesIndex = 0;
+        
+        //id of the last value is inserted into userValuesIndex
+        for(var i = 0; i< numberOfValues; i++){
+            if(userValues[i].id > userValuesIndex){
+                userValuesIndex = userValues[i].id;
+            }
+        }
+        
+        for( var i = 0; i < numberOfValues; i++){
+            if(userValues[i].type == "dexcom"){
+                lastDate = userValues[i].date;
+            }
+        }
+        
+        if(lastDate == null)lastDate = "2018-12-15T16:03:06";
+        
+        console.log(lastDate);
+        
+        fs.readFile(settings.dexcom_data, function(err, data){
+            var dexcomData = JSON.parse(data);
+            var numbersOfDexcomData = dexcomData.egvs.length;
+        
+            for(var i = 0; i < numbersOfDexcomData; i++){
+                if(dexcomData.egvs[i].displayTime == lastDate){
+                    for(var x = (i-1); x >= 0; x--){
+                        userValues.push({
+                            "id" : ++userValuesIndex,
+                            "type" : "dexcom",
+                            "date" : dexcomData.egvs[x].displayTime,
+                            "value" : dexcomData.egvs[x].value
+                        }); 
+                    }
+                }
+            
+               
+            }
+            fs.writeFile(settings.user_values, JSON.stringify(userValues, null, 2));
+        });     
+    });
+});
+
+//GET /userValues
+app.get('/userValues', bodyParser.json(), function(req, res){
+    fs.readFile(settings.user_values, function(err, data){
+        var userValues = JSON.parse(data);
+        res.status(200).send(userValues);
+  });
+});
+
+//GET /userValues/:date
 
 //POST /authorization
 app.post('/authorization', bodyParser.json(), function(req, res){
@@ -35,7 +96,7 @@ app.post('/authorization', bodyParser.json(), function(req, res){
     var newUserAuthorization;
     
     //refresh token
-    fs.readFile(settings.user_Authorization, function(err, data){
+    fs.readFile(settings.user_authorization, function(err, data){
         var userAuthorization = JSON.parse(data);
         var refreshToken = userAuthorization.refresh_token;
         
@@ -62,7 +123,7 @@ app.post('/authorization', bodyParser.json(), function(req, res){
             res.on("end", function () {
                 newUserAuthorization = Buffer.concat(chunks);
                 console.log(newUserAuthorization.toString());
-                fs.writeFile(settings.user_Authorization, JSON.stringify(JSON.parse(newUserAuthorization.toString()), null, 2));
+                fs.writeFile(settings.user_authorization, JSON.stringify(JSON.parse(newUserAuthorization.toString()), null, 2));
                 
             });
         });
@@ -80,23 +141,27 @@ app.post('/authorization', bodyParser.json(), function(req, res){
         });
 });
 
-//POST /dexcomValues
+//POST /dexcomValues/:date
 app.post('/dexcomValues/:date', bodyParser.json(), function(req, res){
-    fs.readFile(settings.user_Authorization, function(err, data){
+    
+
+    fs.readFile(settings.user_authorization, function(err, data){
         var userAuthorization = JSON.parse(data);
         var date = req.params.date;
         console.log(date);
+
         fs.readFile(settings.dexcom_data, function(err, data){
-           
+            var dexcomData = JSON.parse(data);
+
 
             var dexcomOptions = {
                 "method": "GET",
                 "hostname": "api.dexcom.com",
                 "port": null,
-                "path": "/v2/users/self/egvs?startDate=2019-01-10T00:00:00&endDate=" + date,
+                "path": "/v2/users/self/egvs?startDate=2018-12-16T00:00:00&endDate=" + date,
                 "headers": {
                     "authorization": "Bearer " + userAuthorization.access_token,
-                    }
+                }
             };
 
             var req = dexcomHTTP.request(dexcomOptions, function (res) {
@@ -110,15 +175,15 @@ app.post('/dexcomValues/:date', bodyParser.json(), function(req, res){
                     var dexcomValues = Buffer.concat(chunks);
                     console.log(dexcomValues.toString());
                     fs.writeFile(settings.dexcom_data, JSON.stringify(JSON.parse(dexcomValues.toString()), null, 2));
+
                 });
             });
-
             req.end();
-          
         });
     });
-});
 
+
+});
 
 //GET /dexcomValues
 app.get('/dexcomValues', function(req, res){
@@ -130,33 +195,55 @@ app.get('/dexcomValues', function(req, res){
 
 //POST /events
 app.post('/events', bodyParser.json(), function(req, res){
-    fs.readFile(settings.user_events, function(err,data){
-        var events = JSON.parse(data);
-        var numberOfEvents = events.length;
-        var max_index = 0;
+    fs.readFile(settings.user_values, function(err, data){
+        var values = JSON.parse(data);
+        var numberOfValues = values.length;
+        var userValuesIndex = 0;
         
-        //id of the last event is inserted into max_index
-        for(var i = 0; i< numberOfEvents; i++){
-            if(events[i].id > max_index){
-                max_index = events[i].id;
+        //id of the last value is inserted into userValuesIndex
+            for(var i = 0; i< numberOfValues; i++){
+                if(values[i].id > userValuesIndex){
+                    userValuesIndex = values[i].id;
+                }
             }
-        }
         
-        //creat event
-        events.push({
-            "id" : ++max_index,
-            "date" : req.body.date,
-            "value" : req.body.value,
-            "carbohydrates" : req.body.carbohydrates,
-            "be" : req.body.be,
-            "correction" : req.body.correction,
-            "meal_id" : req.body.meal_id,
-            "insulin_units" : req.body.insulin_units,
-            "insulin_type" : req.body.insulin_type
+        fs.readFile(settings.user_events, function(err,data){
+            var events = JSON.parse(data);
+            var numberOfEvents = events.length;
+            var max_index = 0;
+        
+            //id of the last event is inserted into max_index
+            for(var i = 0; i< numberOfEvents; i++){
+                if(events[i].id > max_index){
+                    max_index = events[i].id;
+                }
+            }
+        
+            //creat event
+            events.push({
+                "id" : ++max_index,
+                "date" : req.body.date,
+                "value" : req.body.value,
+                "carbohydrates" : req.body.carbohydrates,
+                "be" : req.body.be,
+                "correction" : req.body.correction,
+                "meal_id" : req.body.meal_id,
+                "insulin_units" : req.body.insulin_units,
+                "insulin_type" : req.body.insulin_type
+            });
+            
+            values.push({
+                "id" : ++userValuesIndex,
+                "type" : "manuell",
+                "date" : req.body.date,
+                "value" : req.body.value
+            });
+            
+            fs.writeFile(settings.user_events, JSON.stringify(events, null, 2));
+            fs.writeFile(settings.user_values, JSON.stringify(values, null, 2));
+            res.status(201).send(events[(max_index-1)]);
+        
         });
-        fs.writeFile(settings.user_events, JSON.stringify(events, null, 2));
-        res.status(201).send(events[(max_index-1)]);
-        
     });
 });
 
